bff5e251a37d13d210dc3ae5c3ed0e0f1dfe0f0a
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-10-20 13:16:00 +0800

    Dashboard has been created for doctor or patent view

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-default">
+            <NavLink className="navbar-brand" to="/"><strong>Doctor <span className="text-secondary">Chamber</span></strong></NavLink>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav ml-auto">
+                <NavLink className="nav-item nav-link" to="/home">Home <span className="sr-only">(current)</span></NavLink>
+                <NavLink className="nav-item nav-link" to="/about">About</NavLink>
+                <NavLink className="nav-item nav-link" to="/services">Dental Services</NavLink>
+                <NavLink className="nav-item nav-link" to="/dashboard">Reviews</NavLink>
+                <NavLink className="nav-item nav-link" to="/blog">Blog</NavLink>
+                <NavLink className="nav-item nav-link" to="/contact">Contact Us</NavLink>
+                </div>
+            </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -6,15 +6,33 @@ import {
 import Home from '../Home/Home';
 import PatientForm from '../PatientForm/PatientForm';
 import BookAppointments from '../BookAppointments/BookAppointments';
+import Navbar from '../Navbar/Navbar';
+import DashboardRoutes from '../Dashboard/DashboardRoutes/DashboardRoutes';
 
 const Routes = () => {
     return (
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/home" component={Home} />
-                <Route path="/booking" component={BookAppointments} />
-                <Route path="/patient-form/patient/:patientKey" component={PatientForm} />
-            </Switch>
+        <Switch>
+            <Route exact path="/">
+                <Navbar></Navbar>
+                <Home></Home>
+            </Route>
+            <Route path="/home">
+                <Navbar></Navbar>
+                <Home></Home>
+            </Route>
+            <Route path="/booking">
+                <Navbar></Navbar>
+                <BookAppointments></BookAppointments>
+            </Route>
+            <Route path="/patient-form/patient/:patientKey">
+                <Navbar></Navbar>
+                <PatientForm></PatientForm>
+            </Route>
+            <Route path="/dashboard">
+                <DashboardRoutes></DashboardRoutes>
+            </Route>
+            <Route path="*"><h1>Not Found The Page</h1></Route>
+        </Switch>
     );
 };
 
